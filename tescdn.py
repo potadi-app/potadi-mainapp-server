5e2f1e67a6ff5ae063935131b3ce07b7ba9adc70
eldivategar <tegareldiva@students.amikom.ac.id>
2024-09-14 21:14:35 +0700

    remove unused tes file

--- tescdn.py
+++
@@ -1,7 +0,0 @@
-from cloudinary import uploader
-
-try:
-    result = uploader.upload("diagnoses/user@example.com/Late_5.jpg")
-    print(result)
-except Exception as e:
-    print(f"Error uploading to Cloudinary: {e}")
